414c576dffa8c3c6c623ae315d0b6f085a429194
Helen Liutongco <bellpickle@gmail.com>
2017-11-20 20:50:11 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express')
 var app = express()
 
-// Build Your Route Here
-
+app.get("/",function(req,resp){
+  const welcomeText="Welcome to Beats by Dre"
+  const category1="For Slow Jams"
+  const cta="Buy some $100 headphones today"
+  const categories=["Making Your Music More Hype","Turning Weak Beats to Strong Beats"]
+  const viewData={
+    "cta":cta,
+    "welcomeText":welcomeText,
+    "category1":category1,
+  }
+  resp.render("index",viewData)
+})
 
 module.exports = app
